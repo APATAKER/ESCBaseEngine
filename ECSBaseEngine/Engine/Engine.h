2a5b4d06b20155615b14655421d65431795a3f4e
dhilip <therealthelip@gmail.com>
2020-03-11 19:29:16 -0400

    added include files

--- ECSBaseEngine/Engine/Engine.h
+++ ECSBaseEngine/Engine/Engine.h
@@ -1,1 +1,22 @@
 #pragma once
+
+#include "../Systems/system.h" // TO BE changed : change to the systems you want
+
+class Engine
+{
+public:
+	Engine();
+	virtual ~Engine();
+
+	bool Initialize();
+	bool Destroy();
+	void Run();
+	void AddSystem(System* s);
+
+private:
+	void Update();
+	void Draw();
+
+	std::vector<System*> mSystemList;
+
+};
--- ECSBaseEngine/Systems/system.h
+++ ECSBaseEngine/Systems/system.h
@@ -1,1 +1,34 @@
 #pragma once
+
+#include "../Helpers/Includes.h"
+#include "../Entities/EntityManager.h"
+
+class System
+{
+public:
+
+	virtual ~System();
+
+	virtual void Process() = 0;
+
+protected:
+	System(std::string name);
+	std::string systemName;
+
+private:
+	System();
+
+};
+
+System::System(std::string name)
+{
+	this->systemName = name;
+}
+
+System::System()
+{
+}
+
+System::~System()
+{
+}
